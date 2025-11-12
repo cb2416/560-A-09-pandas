ed93d94ad9e3a557d0d9bc09b83873a744792a8c
cb2416 <cybarnes@unc.edu>
2025-11-11 23:09:59 -0500

    removed roster bar

--- roster.py
+++ roster.py
@@ -2,8 +2,12 @@
 
 import pandas as pd
 
-roster = ["Bacot", "Davis", "Cadeau"]
-data = pd.DataFrame(roster)
+player = {"Last Name": ["Bacot", "Davis", "Cadeau"],
+          "First Name": ["Armando", "RJ", "Elliot"],
+          "height": [83, 72, 73],
+          "weight": [240, 180, 180]}
+data = pd.DataFrame(player)
 print(data)
+
 '''for player in roster: 
     print(player)'''
